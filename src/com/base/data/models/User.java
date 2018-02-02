d18f482ae7fc77a5408d3519c889c2605af72c99
Alex Kunz <a009k781@ku.edu>
2018-02-01 22:11:26 -0600

    Create user class

---
+++ src/com/base/data/models/User.java
@@ -0,0 +1,43 @@
+public class User
+{
+  String name;
+  String password;
+  boolean isAdmin;
+
+  public User (
+  String name,
+  String password,
+  boolean isAdmin)
+  {
+    this.name = name;
+    this.password = password;
+    this.isAdmin = isAdmin;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  public String getPassword()
+  {
+    return password;
+  }
+  public void setPassword(String password)
+  {
+    this.password = password;
+  }
+
+  public boolean getAdmin()
+  {
+    return isAdmin;
+  }
+  public setAdmin(boolean isAdmin)
+  {
+    this.isAdmin = isAdmin;
+  }
+}
