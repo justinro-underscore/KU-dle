ada2c169b6568530db742e8024f63bb4f8f14e69
Noah Brabec <noah.brabec@gmail.com>
2018-02-01 21:47:55 -0600

    Added setName and get Name

--- src/com/base/data/models/Event.java
+++ src/com/base/data/models/Event.java
@@ -1,3 +1,34 @@
 package com.base.data.models;
 
-public class Event {}
+public class Event
+{
+  private String m_name;
+  private List<User> m_attendees;
+  //TODO private List<datetime> validTimes;
+
+  Event(name, date, a list of valid times);
+
+  /*
+  @pre: A valid string
+  @post: re-assgins the member variable m_name
+  @return: none
+  */
+  public void setName(String name)
+  {
+    m_name = name;
+  }
+
+  /*
+  @pre: none
+  @post: none
+  @return: returns the events name
+  */
+  public String getName()
+  {
+    return(m_name);
+  }
+
+  
+
+
+}
