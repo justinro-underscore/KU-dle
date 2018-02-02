8ad258983a4bcedc12310243bba9a8356148af03
Varun Chadha <vchadha023@gmail.com>
2018-02-01 21:48:34 -0600

    Added Users interface for gson - vchadha

---
+++ src/com/base/data/interfaces/Users.java
@@ -0,0 +1,31 @@
+package com.base.data.models;
+
+public class Users {
+    /*
+    Interface for gson to convert json of users to list of users
+     */
+    private List<User> users;
+
+    /*
+    @Pre: takes in populated list of users
+    @Post: sets list of users to passed in users
+     */
+    public Users(List<User> users) {
+        this.users = users;
+    }
+
+    /*
+    @Return: list of user
+     */
+    public List<User> getUsers() {
+        return users;
+    }
+
+    /*
+    @Pre: takes in list of users
+    @Post: sets list of users to passend in users
+     */
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
