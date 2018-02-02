32fdd99f8dac24b0df679d108707da02a3051669
Varun Chadha <vchadha023@gmail.com>
2018-02-02 09:41:43 -0600

    Added base for saving and reading events - vchadha

--- src/com/base/data/DairyFarmerClient.java
+++ src/com/base/data/DairyFarmerClient.java
@@ -1,24 +1,33 @@
 package com.base.data;
 
+import com.base.data.interfaces.Events;
 import com.base.data.models.Event;
 import com.base.data.models.User;
 import com.google.gson.Gson;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 
 //TODO: add tests
+//TODO: Validate input (no duplicate named events on same day?)
 public class DairyFarmerClient {
     private Gson gson;
     private FileWriter writer;
+    private BufferedReader reader;
 
     public DairyFarmerClient() {
         gson = new Gson();
     }
 
-    public List<Event> getEvents() {
-        return null;
+    //TODO: change to pass in date object?
+    //TODO: Use fileUtil library?
+    public List<Event> getEvents(String date) throws IOException {
+        reader = new BufferedReader(new FileReader("res/events/" + date + ".txt"));
+
+        String json = reader.readLine();
+        Events events = gson.fromJson(json, Events.class);
+
+        return events.getEvents();
     }
 
     public void createEvent(String eventName, String creatorName, List<User> attendees) {
@@ -32,9 +41,12 @@ public class DairyFarmerClient {
 
     }
 
+    //TODO: what to do with users (store data in res/users/)
+
     //TODO: create file writer
+    //TODO: change file name to date (requires from createEvent)
     public void createFile(String json, String fileName) throws IOException {
-        writer = new FileWriter(fileName);
+        writer = new FileWriter("res/events/" + fileName + ".txt");
         writer.write(json);
         writer.close();
     }
