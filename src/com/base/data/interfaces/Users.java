b129543516010511a4a2023deba178c73814ffe3
Noah Brabec <noah.brabec@gmail.com>
2018-02-08 12:41:51 -0600

    Converted Users interface from lists to hashmaps

--- src/com/base/data/interfaces/Users.java
+++ src/com/base/data/interfaces/Users.java
@@ -2,34 +2,34 @@ package com.base.data.interfaces;
 
 import com.base.data.models.User;
 
-import java.util.List;
+import java.util.*;
 
 public class Users {
     /*
-    Interface for gson to convert json of users to list of users
+    Interface for gson to convert json of users to a HashMap of users
      */
-    private List<User> users;
+    private HashMap<String, User> user_list = new HashMap<>();
 
     /*
-    @Pre: takes in populated list of users
-    @Post: sets list of users to passed in users
+    @Pre: takes in populated HashMap of users
+    @Post: sets HashMap of users to passed in users
      */
-    public Users(List<User> users) {
-        this.users = users;
+    public Users(HashMap<String, User> users) {
+        this.user_list = users;
     }
 
     /*
-    @Return: list of user
+    @Return: HashMap of user
      */
-    public List<User> getUsers() {
-        return users;
+    public HashMap<String, User> getUsers() {
+        return this.user_list;
     }
 
     /*
-    @Pre: takes in list of users
-    @Post: sets list of users to passend in users
+    @Pre: takes in hashmap of users
+    @Post: sets hashmap of users to passend in users
      */
-    public void setUsers(List<User> users) {
-        this.users = users;
+    public void setUsers(HashMap<String, User> users) {
+        this.user_list = users;
     }
 }
