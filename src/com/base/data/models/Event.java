d0e576d0cb5f5b86beaec742e269d645f40eb20a
Varun Chadha <vchadha023@gmail.com>
2018-02-09 12:32:25 -0600

    Added documentation to Event.java - vchadha

--- src/com/base/data/models/Event.java
+++ src/com/base/data/models/Event.java
@@ -13,10 +13,10 @@ public class Event
   private List<Time> times;
 
   /*
-  @pre: String representing eventName, creator name, date, and a list of times
-  @post: assign local variables to passed in values
-  @return: none
-  */
+  * @pre: String representing eventName, creator name, date, and a list of times
+  * @post: assign local variables to passed in values
+  * @return: none
+   */
   public Event(String eventName, String creatorName, LocalDate date, List<Time> times) {
     this.eventName = eventName;
     this.creatorName = creatorName;
@@ -24,46 +24,76 @@ public class Event
     this.times = times;
   }
 
-/*
-  @pre: A valid string
-  @post: re-assgins the member variable m_name
-  @return: none
-  */
+  /*
+  * @pre: none
+  * @post: none
+  * @return: returns the events name
+   */
+  public String getEventName()
+  {
+    return(eventName);
+  }
+
+  /*
+  * @pre: A valid string
+  * @post: re-assgins the member variable eventName
+  * @return: none
+   */
   public void setEventName(String eventName)
   {
     eventName = eventName;
   }
 
   /*
-  @pre: none
-  @post: none
-  @return: returns the events name
-  */
-  public String getEventName()
-  {
-    return(eventName);
-  }
-
+  * @pre: none
+  * @post: none
+  * @return: returns the event creator name
+   */
   public String getCreatorName() {
     return creatorName;
   }
 
+  /*
+  * @pre: A valid string
+  * @post: re-assgins the member variable creatorName
+  * @return: none
+   */
   public void setCreatorName(String creatorName) {
     this.creatorName = creatorName;
   }
 
+  /*
+  * @pre: none
+  * @post: none
+  * @return: returns the date of the event
+   */
   public LocalDate getDate() {
     return date;
   }
 
+  /*
+  * @pre: A valid date
+  * @post: re-assgins the member variable date
+  * @return: none
+   */
   public void setDate(LocalDate date) {
     this.date = date;
   }
 
+  /*
+  * @pre: none
+  * @post: none
+  * @return: returns the list of times for the event
+   */
   public List<Time> getTimes() {
     return times;
   }
 
+  /*
+  * @pre: A valid list of times
+  * @post: re-assgins the member variable times
+  * @return: none
+   */
   public void setTimes(List<Time> times) {
     this.times = times;
   }
