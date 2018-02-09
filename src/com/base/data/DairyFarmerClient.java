d9499d915e0328383adaeb6d03123f0a9314d0f7
Varun Chadha <vchadha023@gmail.com>
2018-02-09 09:30:39 -0600

    Upaded docs, event txt, and comment style for DairyFarmerClient - vchadha

--- src/com/base/data/DairyFarmerClient.java
+++ src/com/base/data/DairyFarmerClient.java
@@ -21,19 +21,31 @@ public class DairyFarmerClient {
     private HashMap<LocalDate, List<Event>> events;
     private HashMap<String, User> users;
 
+    /*
+    * @pre: nothing
+    * @post: initializes gson object
+    * @return: nothing
+     */
     public DairyFarmerClient() {
         gson = new Gson();
     }
 
+    /*
+    * @pre: requires a date to get specific events for that day
+    * @post: nothing
+    * @return: list of events for the provided date
+     */
     public List<Event> getEvents(LocalDate date) throws IOException {
         //TODO: not sure if we need to check containsKey if event list does not exist
 
         return events.get(date);
     }
 
-    //@pre: requires an eventName, creatorName, date and a list of times
-    //@post: creates event and adds to hashmap of events
-    //@return: nothing
+    /*
+    * @pre: requires an eventName, creatorName, date and a list of times
+    * @post: creates event and adds to hashmap of events
+    * @return: nothing
+     */
     public void createEvent(String eventName, String creatorName, LocalDate date, List<Time> times) throws IOException {
         //TODO: maybe have list of times with their own list of attendees (that way we know who is available at what time)?
         //TODO: maybe add to list -> have list of events for each day and get specific by creator
@@ -41,7 +53,7 @@ public class DairyFarmerClient {
         Event event = new Event(eventName, creatorName, date, times);
 
         if (events.containsKey(date)) {
-            getEvents(date).add(event);
+            events.get(date).add(event);
         } else {
             List<Event> tempEvents = new ArrayList<>();
             tempEvents.add(event);
@@ -50,9 +62,11 @@ public class DairyFarmerClient {
         }
     }
 
-    //@pre: requires a date, and event name
-    //@post: if events exists, delete event from hashmap
-    //@return: nothing
+    /*
+    * @pre: requires a date, and event name
+    * @post: if events exists, delete event from hashmap
+    * @return: nothing
+     */
     public void deleteEvent(LocalDate date, String eventName) throws IOException {
         List<Event> events = getEvents(date);
         for(Event event : events){
@@ -63,39 +77,53 @@ public class DairyFarmerClient {
 
     }
 
-    //@pre: requires a username, and password, and if the user is an admin
-    //@post: checks if user exits, then adds it to the user hashmap if it doesnt
-    //@return: nothing (may change to a bool)
+    /*
+    * @pre: requires a username, and password, and if the user is an admin
+    * @post: checks if user exits, then adds it to the user hashmap if it doesnt
+    * @return: nothing (may change to a bool)
+     */
     public void createUser(User user)
     {
         //TODO Throw error stating that username is taken
 
     }
 
-    //@pre: requires a username
-    //@post: finds and removes a user from the know user list
-    //@return: //TODO I think this should be a bool to see if a user was deleted or not.
+    /*
+    * @pre: requires a username
+    * @post: finds and removes a user from the know user list
+    * @return: //TODO I think this should be a bool to see if a user was deleted or not.
+     */
     public void deleteUser(User user)
     {
       //TODO make sure this doesnt break
 
     }
 
-    //Is this supposed to search for and return some user?
+    /*
+    * @pre: nothing
+    * @post: nothing
+    * @return: returns hashmap of users
+     */
     public List<User> getUsers() {
         return null;
     }
 
-    //TODO: write user and events hashmap to file
+    /*
+    * @pre: String of json data and file name
+    * @post: writes json data to file
+    * @return: nothing
+     */
     public void createFile(String json, String fileName) throws IOException {
         writer = new FileWriter(fileName);
         writer.write(json);
         writer.close();
     }
 
-    //@pre: Requires the filename containing user information
-    //@post: initializes a file scanner that seperates terms by ",". Adds user information to Hashaps
-    //@return: nothing
+    /*
+    * @pre: Requires the filename containing user information
+    * @post: creates a hashmap of users
+    * @return: nothing
+    */
     public void initUsers() throws IOException {
         File userFile = new File("res/users.txt");
         if (!userFile.exists()) {
@@ -111,6 +139,11 @@ public class DairyFarmerClient {
         users = tempUsers.getUsers();
     }
 
+    /*
+    * @pre: nothing
+    * @post: creates a hashmap of events
+    * @return: nothing
+    */
     public void initEvents() throws IOException {
         //TODO: maybe change path to a constant?
         File eventsFile = new File("res/events.txt");
@@ -127,12 +160,22 @@ public class DairyFarmerClient {
         events = tempEvents.getEvents();
     }
 
+    /*
+    * @pre: nothing
+    * @post: converts events to json and saves to file
+    * @return: nothing
+    */
     public void saveEvents() throws IOException {
         String eventJson = gson.toJson(events);
 
         createFile(eventJson, "res/events.txt");
     }
 
+    /*
+    * @pre: nothing
+    * @post: converts user data to json and save to file
+    * @return: nothing
+    */
     public void saveUsers() throws IOException {
         String userJson = gson.toJson(users);
 
