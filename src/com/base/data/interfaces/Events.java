1fafc164e176d3d562deb2611f7d48e166be9f06
Varun Chadha <vchadha023@gmail.com>
2018-02-09 12:38:02 -0600

    Added documentation to Events class - vchadha

--- src/com/base/data/interfaces/Events.java
+++ src/com/base/data/interfaces/Events.java
@@ -9,14 +9,29 @@ import java.util.List;
 public class Events {
     private HashMap<LocalDate, List<Event>> events;
 
+    /*
+    * @pre: Takes in a hashmap of events with date as key
+    * @post: assign local variable to passed in value
+    * @return: nothing
+     */
     public Events(HashMap<LocalDate, List<Event>> events) {
         this.events = events;
     }
 
+    /*
+    * @pre: nothing
+    * @post: nothing
+    * @return: events
+     */
     public HashMap<LocalDate, List<Event>> getEvents() {
         return events;
     }
 
+    /*
+    * @pre: takes in a hashmap of events with date as key
+    * @post: re-assigns the member variable events
+    * @return: nothing
+     */
     public void setEvents(HashMap<LocalDate, List<Event>> events) {
         this.events = events;
     }
